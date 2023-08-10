16d2a0876a0d0115a9d1e4a71c4a94a4e6feef68
unknown <eagila926@gmail.com>
2023-08-10 11:04:36 -0500

    Creacion de vistas, cambio de la tabla inventario

--- ortomed/cotizador/views.py
+++ ortomed/cotizador/views.py
@@ -5,3 +5,26 @@ from django.shortcuts import render
 def login(request):
     return render( request, 'login.html')
 
+def comercial(request):
+    return render( request, 'comercial.html')
+
+def dashboard(request):
+    return render( request, 'dashboard.html')
+
+def etiquetas(request):
+    return render(request, 'etiquetas.html')
+
+def facturas(request):
+    return render(request, 'facturas.html')
+
+def formula(request):
+    return render(request, 'formula.html')
+
+def inventario(request):
+    return render(request, 'inventario.html')
+
+def pedidos(request):
+    return render(request, 'pedidos.html')
+
+def reportes(request):
+    return render(request, 'reportes.html')
