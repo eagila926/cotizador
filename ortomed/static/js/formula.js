cb0d08bc384648890a44b8e2488a4e46aec9cc37
unknown <eagila926@gmail.com>
2023-09-05 16:29:55 -0500

    Realizacion de calculos de masas y volumen final

--- ortomed/static/js/formula.js
+++ ortomed/static/js/formula.js
@@ -1,12 +1,42 @@
-var tblFormu;
+var tblformu;
 var formus = {
     items: {
-        cod_cliente: '',
+        cod_formula: '',
         nombre: '',
         subtotal: 0.00,
-        iva: 0.00,
-        total: 0.00,
+        dosis: 0,
         activos: []
+    },
+    calculate_invoice: function () {
+        var mfinal = 0.00;
+        var vfinal = 0.00;
+        
+
+        
+        $.each(this.items.activos, function (pos, dict) {
+            dict.pos = pos;
+            if(dict.unidad_compra === 'mg' || dict.unidad_compra === 'UI'){
+                dict.cant = dict.cant/1000
+            }else if(dict.unidad_compra === 'mcg'){
+                dict.cant = dict.cant/1000000
+            }else{
+                dict.cant = dict.cant
+            }
+            //quedaste aqui
+            mfinal = dict.cant * parseFloat(dict.factor);
+
+            
+        });
+
+        console.log('masa final',dict.mfinal)
+        this.items.subtotal = subtotal;
+        this.items.iva = this.items.subtotal * iva;
+        this.items.total = this.items.subtotal + this.items.iva;
+
+
+        $('input[name="subtotal"]').val(this.items.subtotal.toFixed(2));
+        $('input[name="ivacalc"]').val(this.items.iva.toFixed(2));
+        $('input[name="total"]').val(this.items.total.toFixed(2));
     },
     add: function (item) {
         this.items.activos.push(item);
@@ -14,18 +44,19 @@ var formus = {
     },
     list: function () {
 
-        tblFormu = $('#tblFormu').DataTable({
+        tblformu = $('#tblformu').DataTable({
             responsive: true,
             autoWidth: false,
             destroy: true,
             data: this.items.activos,
             columns: [
-                {"data": "id"},
+                {"data": "cod_inven"},
                 {"data": "descripcion"},
                 {"data": "factor"},
-                {"data": "densidad"},
                 {"data": "cant"},
-                {"data": "cant"},
+                {"data": "unidad_compra"},
+                {"data": "valor_venta"},
+                {"data": "valor_costo"},
             ],
             columnDefs: [
                 {
@@ -37,15 +68,7 @@ var formus = {
                     }
                 },
                 {
-                    targets: [-3],
-                    class: 'text-center',
-                    orderable: false,
-                    render: function (data, type, row) {
-                        return '$' + parseFloat(data).toFixed(2);
-                    }
-                },
-                {
-                    targets: [-2],
+                    targets: [3],
                     class: 'text-center',
                     orderable: false,
                     render: function (data, type, row) {
@@ -53,13 +76,30 @@ var formus = {
                     }
                 },
                 {
-                    targets: [-1],
+                    targets: [4],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<select id="unidad_compra" class="form-control form-control-sm select-sm" autocomplete="off" > <option value="g"> g </option> <option value="mg"> mg </option> <option value="mcg"> mcg </option> <option value="UI"> UI </option>';
+                    }
+                },
+                {
+                    targets: [5],
                     class: 'text-center',
                     orderable: false,
                     render: function (data, type, row) {
                         return '$' + parseFloat(data).toFixed(2);
                     }
                 },
+                {
+                    targets: [6],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '$' + parseFloat(data).toFixed(2);
+                    }
+                },
+                
             ],
             rowCallback(row, data, displayNum, displayIndex, dataIndex) {
 
@@ -84,7 +124,7 @@ $(function () {
         language: 'es'
     });
 
-    // search products
+    // search activos
 
     $('input[name="search"]').autocomplete({
         source: function (request, response) {
@@ -117,7 +157,7 @@ $(function () {
     });
 
     $('.btnRemoveAll').on('click', function () {
-        if (vents.items.products.length === 0) return false;
+        if (formus.items.activos.length === 0) return false;
         alert_action('Notificación', '¿Estas seguro de eliminar todos los items de tu detalle?', function () {
             formus.items.activos = [];
             formus.list();
@@ -125,21 +165,21 @@ $(function () {
     });
 
     // event cant
-    $('#tblProducts tbody')
+    $('#tblformu tbody')
         .on('click', 'a[rel="remove"]', function () {
-            var tr = tblProducts.cell($(this).closest('td, li')).index();
+            var tr = tblformu.cell($(this).closest('td, li')).index();
             alert_action('Notificación', '¿Estas seguro de eliminar el producto de tu detalle?', function () {
-                vents.items.products.splice(tr.row, 1);
-                vents.list();
+                formus.items.activos.splice(tr.row, 1);
+                formus.list();
             });
         })
         .on('change', 'input[name="cant"]', function () {
             console.clear();
             var cant = parseInt($(this).val());
-            var tr = tblProducts.cell($(this).closest('td, li')).index();
-            vents.items.products[tr.row].cant = cant;
-            vents.calculate_invoice();
-            $('td:eq(5)', tblProducts.row(tr.row).node()).html('$' + vents.items.products[tr.row].subtotal.toFixed(2));
+            var tr = tblformu.cell($(this).closest('td, li')).index();
+            formus.items.activos[tr.row].cant = cant;
+            formus.calculate_invoice();
+            $('td:eq(5)', tblformu.row(tr.row).node()).html('$' + formus.items.activos[tr.row].subtotal.toFixed(2));
         });
 
     $('.btnClearSearch').on('click', function () {
@@ -150,16 +190,16 @@ $(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
 
-        if(vents.items.products.length === 0){
+        if(formus.items.activos.length === 0){
             message_error('Debe al menos tener un item en su detalle de venta');
             return false;
         }
 
-        vents.items.date_joined = $('input[name="date_joined"]').val();
-        vents.items.cli = $('select[name="cli"]').val();
+        formus.items.date_joined = $('input[name="date_joined"]').val();
+        formus.items.cli = $('select[name="cli"]').val();
         var parameters = new FormData();
         parameters.append('action', $('input[name="action"]').val());
-        parameters.append('vents', JSON.stringify(vents.items));
+        parameters.append('formus', JSON.stringify(formus.items));
         submit_with_ajax(window.location.pathname, 'Notificación', '¿Estas seguro de realizar la siguiente acción?', parameters, function () {
             location.href = '/erp/sale/list/';
         });
